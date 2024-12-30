e2551d7238c77d1ef197f1341293312ec4eca668
dalalbinhamoud <dalalbinhamoud@gmail.com>
2024-12-30 19:18:40 +0300

    Initial Commit

---
+++ ParkAround/ParkAroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkAroundApp.swift
+//  ParkAround
+//
+//  Created by Dalal Macbook on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ParkAroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
